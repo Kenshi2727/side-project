15f66fba63dbe7eda74e4cb88837904ab069255e
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2024-12-04 15:47:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "book-notes",
-    password: "<password>",
+    password: "spiralhelix27",
     port: 5432
 })
 
